a49a226f22dcab38c6e962f02e5ab6afffd56f41
czkirilo <caiquez.kirilo@hotmail.com>
2022-08-16 21:17:04 -0300

    ER5_B

--- UC09_BE_UC12_FULL/SA2/Sistema/Program.cs
+++ UC09_BE_UC12_FULL/SA2/Sistema/Program.cs
@@ -5,61 +5,65 @@ namespace Sistema
     {
         static void Main(string[] args)
         {
-            /*
-            PessoaFisica pf = new PessoaFisica();
-            pf.endereco = end;
-            pf.nome = "Caique Zaneti";
-            pf.cpf = "6565151";
-            pf.dataNascimento = new DateTime (1994, 05, 19);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(@$"
+=================================================== 
+!                                                 !          
+!                                                 !
+!         Seja Bem Vindo ao nosso Sistema         !
+!           de Cadastro de Pessoa                 !
+!             Fisica e Juridica                   !
+!                                                 !
+!                                                 !
+===================================================               
+");
 
-        //interpolação
-        Console.WriteLine($"O {pf.nome} mora na {pf.endereco.logradouro} numero {pf.endereco.numero}");
-        //concatenação
-        Console.WriteLine("O " + pf.nome + " mora na " + pf.endereco.logradouro + " número " + pf.endereco.numero
-);
-        //forma não-inteligente
-        Console.WriteLine(pf.endereco.logradouro);
-        Console.WriteLine(pf.endereco.numero);
-        Console.WriteLine(pf.endereco.complemento);
+            Console.Write("iniciando ");
+            Thread.Sleep(500);
 
-                
-        bool idadeValida = pf.ValidarDataNascimento(pf.dataNascimento);
- 
-        if (idadeValida == true)
-        {
-            System.Console.WriteLine("Parabens, você é velho o suficiente para acessar o sistema");
-        }else{
-            Console.WriteLine("Você ainda assiste felipe neto que eu sei");
-        }
-    
-*/
-
-            PessoaJuridica pj = new PessoaJuridica();
-            
-            Endereco end = new Endereco();
-            end.logradouro = "Rua X";
-            end.numero = 100;
-            end.complemento = "Qualquer coisa";
-            end.enderecoComercial = true;
-
-            pj.endereco = end;
-            pj.cnpj = "12345678990001";
-            pj.RazaoSocial = "Pessoa Juridica";
-
-            if(pj.ValidarCNPJ(pj.cnpj)){
-                Console.WriteLine("cnpj valido");
-            }else{
-                Console.WriteLine("cnpj invalido");
+            for (var contador = 0; contador < 10; contador++)
+            {
+                Console.Write("#");
+                Thread.Sleep(500);
             }
-            
+            Console.Clear();
 
+        string? opcao;
 
+            do
+            {
+                Console.WriteLine(@$"
+=================================================== 
+!          Escolha uma das opcoes abaixo          !          
+=================================================== 
+!              1 - Pessoa Fisica                  !
+!              2 - Pessoa Juridica                !
+!                                                 !
+!              0 - Sair                           !
+!                                                 !
+===================================================               
+");
 
+                opcao = Console.ReadLine();
 
+                switch (opcao)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "0":
+                        break;
+                    default:
+                        Console.WriteLine("Opcao invalida, por favor digite uma das opcoes apresentadas");
+                        break;
+                }
+            } while (opcao != "0");
 
+                        Console.ResetColor();
         }
     }
 }
 
-
+// pagina 5
 // auto ident = shift + alt + F
