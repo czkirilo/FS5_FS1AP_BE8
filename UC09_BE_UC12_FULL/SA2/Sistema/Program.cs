0e98429273659555299a8411c18214183590f27a
czkirilo <caiquez.kirilo@hotmail.com>
2022-08-24 18:55:25 -0300

    ER6

--- UC09_BE_UC12_FULL/SA2/Sistema/Program.cs
+++ UC09_BE_UC12_FULL/SA2/Sistema/Program.cs
@@ -5,7 +5,25 @@ namespace Sistema
     {
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Green;
+
+            static void BarraCarregamento(string texto)
+            {
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Green;
+
+                Console.Write(texto);
+                Thread.Sleep(500);
+
+                for (var contador = 0; contador < 10; contador++)
+                {
+                    Console.Write("$");
+                    Thread.Sleep(500);
+                }
+
+            }
+
+
+
             Console.WriteLine(@$"
 =================================================== 
 !                                                 !          
@@ -17,18 +35,9 @@ namespace Sistema
 !                                                 !
 ===================================================               
 ");
+           // BarraCarregamento("Iniciando ");
 
-            Console.Write("iniciando ");
-            Thread.Sleep(500);
-
-            for (var contador = 0; contador < 10; contador++)
-            {
-                Console.Write("#");
-                Thread.Sleep(500);
-            }
-            Console.Clear();
-
-        string? opcao;
+            string? opcao;
 
             do
             {
@@ -49,10 +58,48 @@ namespace Sistema
                 switch (opcao)
                 {
                     case "1":
+                        Endereco endPf = new Endereco();
+
+                        endPf.logradouro = "Rua Niteroi";
+                        endPf.numero = 180;
+                        endPf.complemento = "Escola SENAI Paulo Skaf";
+                        endPf.enderecoComercial = false;
+
+                        PessoaFisica Lucas = new PessoaFisica();
+                        Lucas.cpf = "651683864546";
+                        Lucas.dataNascimento = new DateTime(2004, 08, 21);
+                        Lucas.endereco = endPf;
+                        Lucas.nome = "Lucas Rodriguez Sinni";
+
+                        Console.WriteLine(@$"Nome: {Lucas.nome}
+CPF: {Lucas.cpf} 
+Nascido em {Lucas.dataNascimento.ToString("dd/MM/yyyy")}
+Endereço: {endPf.logradouro}, {endPf.numero}");
+
+                        
+                        Console.WriteLine();
                         break;
                     case "2":
+                        Endereco endPj = new Endereco();
+
+                        endPj.logradouro = "Rua Niteroi";
+                        endPj.numero = 180;
+                        endPj.complemento = "Escola SENAI Paulo Skaf";
+                        endPj.enderecoComercial = true;
+
+                        PessoaJuridica pj = new PessoaJuridica();
+                        pj.cnpj = "0104115487";
+                        pj.endereco = endPj;
+                        pj.RazaoSocial = "Pessoa Juridica";
+                        pj.nome = "Jequiti";
+
+                        Console.WriteLine(pj.cnpj);
                         break;
+
                     case "0":
+                        Console.WriteLine("Obrigado por utilizar o nosso sistema");
+                      //  BarraCarregamento("Finalizando ");
+
                         break;
                     default:
                         Console.WriteLine("Opcao invalida, por favor digite uma das opcoes apresentadas");
@@ -60,7 +107,7 @@ namespace Sistema
                 }
             } while (opcao != "0");
 
-                        Console.ResetColor();
+            Console.ResetColor();
         }
     }
 }
